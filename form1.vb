e512899fb6d81f5dceb105384cbe765d5b00edc2
Annie <annetnatukunda31@gmail.com>
2024-10-03 17:09:41 +0300

    Create form1.vb

---
+++ form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        Dim name As String
+        Dim age As Integer
+        Dim favcolor As String
+        'diplay name age and favcolor
+        name = txtName.Text()
+        age = txtAge.Text()
+        favcolor = txtColor.Text()
+        'diplay msg box
+        MessageBox.Show(name & age & favcolor)
+    End Sub
+End Class
